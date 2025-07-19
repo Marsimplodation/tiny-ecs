08c91b2dc39ce1d6a2ec218134a645616d4d5625
Marius Werkmeister <m.werkmeister@tu-braunschweig.de>
2025-07-19 09:49:30 +0200

    example

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,32 @@
-#include <cstdio>
 #define ECS_IMPLEMENTATION
 #include "tiny_ecs.h"
 
+struct Width{float w;};
+struct Height{float h;};
+struct Radius{float r;};
+struct Area{float a;};
 
-const float gravity = 9.81f;
-const float timeStep = 0.016f;
-struct Position{float x, y, z;};
-struct Velocity{float dx, dy, dz;};
-struct Acceleration{float ddx, ddy, ddz;};
+typedef struct {
+    float x, y;
+}  Position, Velocitiy;
+
+
 
 int main (int argc, char *argv[]) {
-	ECS::registerType<Position>();
-	ECS::registerMultipleTypes<Velocity, Acceleration>();
-	auto ball = ECS::newEntity();
+    ECS::registerMultipleTypes<Position, Velocitiy>();
 
-	ECS::addMultipleComponents(ball, Position{0, 500, 0}, Acceleration{0,30,0});
-	ECS::addComponent(ball, Velocity{0,30,0});
-	//gravity system with euler solver
-	for(int i = 0; i < 100; ++i) {
-		//multiple components
-		auto refs = ECS::getMultipleComponents<Position, Velocity>(ball);
-		if(!refs.isValid()) continue;
-		auto [position, velocity] = refs.unwrap();
+    EntityID entitiy1 = ECS::newEntity();
+    ECS::addMultipleComponents(entitiy1, Position{1,2}, Velocitiy{0.1, 0.05});
+    EntityID entitiy2 = ECS::newEntity();
+    ECS::addComponent(entitiy2, Position{2,2});
+    ECS::addComponent(entitiy2, Velocitiy{2,2});
 
-		//single components
-		auto accRef = ECS::getComponent<Acceleration>(ball);
-		if(!accRef.isValid()) continue;
-		auto & acc = *accRef;
-
-		position.x += velocity.dx * timeStep;
-		position.y += velocity.dy * timeStep;
-		position.z += velocity.dz * timeStep;
-		velocity.dy -= gravity * timeStep;
-		printf("%f %f %f\n", position.x, position.y, position.z);
-		printf("%f\n", acc.ddy); 
-	}
-	
-	return 0;
+    //systems
+    for(auto e : ECS::allEntitiesWith<Position, Velocitiy>()) {
+        auto [p, v] = ECS::getMultipleComponents<Position, Velocitiy>(e).unwrap();
+        p.x += v.x;
+        p.y += v.y;
+    }
+    
+    return 0;
 }
